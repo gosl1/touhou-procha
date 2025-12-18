b4b41f8c22c3d184be862b716a81c37843440e57
gosl <jetzrav15@gmail.com>
2025-12-18 23:17:03 +0800

    new

--- src/touhou/Reward.java
+++ src/touhou/Reward.java
@@ -1,16 +1,9 @@
 package touhou;
 public class Reward {
     private String rewardName;
-    // REMOVED: private String description; // Description field removed
-    // REMOVED: private String category; // Category field removed
 
-    // REMOVED: description and category parameters from constructor
-    // BEFORE: public Reward(String rewardName, String description, String category){
-    // AFTER: Only name
     public Reward(String rewardName){
         this.rewardName = rewardName;
-        // REMOVED: this.description = description; // No more description
-        // REMOVED: this.category = category; // No more category
     }
 
     //Getter
@@ -18,28 +11,11 @@ public class Reward {
         return rewardName;
     }
     
-    // REMOVED: getDescription() method
-    // public String getDescription(){
-    //     return description;
-    // }
-    
-    // REMOVED: getCategory() method
-    // public String getCategory(){
-    //     return category;
-    // }
 
     //Setter
     public void setName(String name){
         this.rewardName = name;
     }
 
-    // REMOVED: setDescription() method
-    // public void setDescription(String desc){
-    //     this.description = desc;
-    // }
 
-    // REMOVED: setCategory() method
-    // public void setCategory(String category){
-    //     this.category = category;
-    // }
 }
