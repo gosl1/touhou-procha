f267921fa528bdf6b40e4ebbe162196d9e213535
gosl <jetzrav15@gmail.com>
2025-12-18 23:14:32 +0800

    yeah i maybe changed something

--- src/touhou/Roulette.java
+++ src/touhou/Roulette.java
@@ -6,6 +6,7 @@ public class Roulette {
 
     private TaskManager manager;
     private Random random;
+    private Task lastSelectedTask;
 
     public Roulette(TaskManager manager) {
         this.manager = manager;
@@ -15,7 +16,23 @@ public class Roulette {
     // Returns a random task, or null if no tasks exist
     public Task spin() {
         if (manager.getTasks().isEmpty()) return null;
+
+        if (lastSelectedTask != null){
+        lastSelectedTask.setRouletteBoosted(false);
+        }
+
         int index = random.nextInt(manager.getTasks().size());
-        return manager.getTasks().get(index);
+        lastSelectedTask = manager.getTasks().get(index);
+
+                // Mark this task as boosted
+        lastSelectedTask.setRouletteBoosted(true);
+        
+
+        return lastSelectedTask;
     }
+
+    public Task getLastSelected() {
+            return lastSelectedTask;
+        }
+
 }
--- src/touhou/Task.java
+++ src/touhou/Task.java
@@ -1,18 +1,16 @@
 package touhou;
 public class Task {
     private String taskName;
-    // REMOVED: private String description; // Description field removed
+
     private String category;
     private boolean done;
+    private boolean rouletteBoosted;  // NEW FIELD
 
-    // REMOVED: description parameter from constructor
-    // BEFORE: public Task(String taskname, String description, String category){
-    // AFTER: Only name and category
     public Task(String taskname, String category){
         this.taskName = taskname;
-        // REMOVED: this.description = description; // No more description
         this.category = category;
         this.done = false;
+        this.rouletteBoosted = false;
     }
 
     //Getters
@@ -20,10 +18,7 @@ public class Task {
         return taskName;
     }
     
-    // REMOVED: getDescription() method
-    // public String getDescription(){
-    //     return description;
-    // }
+
     
     public String getCategory(){
         return category;
@@ -37,15 +32,21 @@ public class Task {
         this.taskName = name;
     }
 
-    // REMOVED: setDescription() method
-    // public void setDescription(String desc){
-    //     this.description = desc;
-    // }
+
 
     public void setCategory(String category){
         this.category = category;
     }
     public void markDone(){
         this.done = true;
+    }
+
+        // Add these methods to Task.java:
+    public boolean isRouletteBoosted() {
+        return rouletteBoosted;
+    }
+    
+    public void setRouletteBoosted(boolean boosted) {
+        this.rouletteBoosted = boosted;
     }
 }
